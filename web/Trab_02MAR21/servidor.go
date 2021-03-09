b385c74edde32a33ba50a3c3f8acb645f9875dc1
Davi <davihugomp@gmail.com>
2021-03-08 23:29:08 -0300

    Added go server

---
+++ web/Trab_02MAR21/servidor.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New())
+	app.Post("/login", loginHandler)
+
+	app.Listen(":4000")
+}
+
+func loginHandler(c *fiber.Ctx) error {
+	//Receber os dados
+	type Dados struct {
+		Name     string
+		Mail     string
+		Password string
+	}
+
+	loginInput := new(Dados)
+	c.BodyParser(loginInput)
+
+	return c.JSON(loginInput)
+}
